0f8b9153f4fef98eb45c9169dd5fef684da3d011
axeldinh <axeldvc@gmail.com>
2021-05-06 15:27:03 +0200

    commit

--- Test.m
+++ Test.m
@@ -1,13 +1,11 @@
-clc
-close all
-clear all
-
 format long
 
 sizes = [8, 16, 32, 64, 128, 256, 512, 1024];
 errors = zeros(length(sizes));
-times_my = zeros(length(sizes));
-times_mat = zeros(length(sizes));
+times_schur = zeros(length(sizes));
+times_recon = zeros(length(sizes));
+times_mysylv = zeros(length(sizes));
+times_matsylv = zeros(length(sizes));
 
 for i = 1:length(sizes)
     
@@ -16,21 +14,30 @@ for i = 1:length(sizes)
         M = sizes(i);
         N = sizes(j);
         
-        A = schur(rand(M));
-        B = schur(rand(N));
-        
-        
+        A = rand(M);
+        B = rand(N);
         C = rand(M, N);
         
         t = tic();
-        X1 = rtrsyst(A, B, C, 64, 64);
+        [UA, TA] = schur(A);
+        [UB, TB] = schur(B);
         time = toc(t);
-        times_my(i, j) = time;
+        times_schur(i,j) = time;
+        
+        t = tic();
+        X1 = rtrsyst(TA, TB, UA*C*UB', 64, 64);
+        time = toc(t);
+        times_mysylv(i, j) = time;
+        
+        t = tic();
+        X1 = UA*X1*UB';
+        time = toc(t);
+        times_recon(i,j) = time;
         
         t = tic();
         X2 = sylvester(A, -B, C);
         time = toc(t);
-        times_mat(i, j) = time;
+        times_matsylv(i, j) = time;
         
         errors(i,j) = norm(X1 - X2);
         
@@ -41,8 +48,14 @@ for i = 1:length(sizes)
     
     figure()
     title("M = " + num2str(sizes(i)))
-    loglog(sizes, times_my(i, :), sizes, times_mat(i, :))
+    loglog(sizes, times_mysylv(i, :) + times_schur(i,:) + times_recon(i,:), sizes, times_matsylv(i, :))
     xlabel("N")
     ylabel("times")
     legend("My Code", "Matlab")
 end
+
+% surf(sizes, sizes, times_mysylv + times_schur + times_recon)
+% hold on
+% surf(sizes, sizes, times_matsylv)
+
+xscale('log')
