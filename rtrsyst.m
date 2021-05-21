5148b7b708868d8c26bbfcf115fc759d86cc5bb6
axeldinh <axeldvc@gmail.com>
2021-05-21 14:36:19 +0200

    Modif no X alloc

--- rtrsyst.m
+++ rtrsyst.m
@@ -1,4 +1,4 @@
-function [X] = rtrsyst(A,B,C, mb, nb)
+function [C] = rtrsyst(A,B,C, mb, nb)
 
 M = length(A);
 N = length(B);
@@ -13,32 +13,29 @@ else
         
         newM = newdims(A);
         
-        X2 = rtrsyst(A(newM+1:end, newM+1:end), B, C(newM+1:end, :), mb, nb);
-        C1 = gemm(- A(1:newM, newM +1:end), X2, C(1:newM, :));
-        X1 = rtrsyst(A(1:newM, 1:newM), B, C1, mb, nb);
-        X = [X1;X2];
+        C(newM+1:end, :) = rtrsyst(A(newM+1:end, newM+1:end), B, C(newM+1:end, :), mb, nb);
+        C(1:newM, :) = gemm(- A(1:newM, newM +1:end), C(newM+1:end, :), C(1:newM, :));
+        C(1:newM, :) = rtrsyst(A(1:newM, 1:newM), B, C(1:newM, :), mb, nb);
         
     elseif M <= N/2
         
         newN = newdims(B);
         
-        X1 = rtrsyst(A, B(1:newN, 1:newN), C(:, 1:newN), mb, nb);
-        C2 = gemm(X1, B(1:newN, newN+1:end), C(:, newN+1:end));
-        X2 = rtrsyst(A, B(newN+1:end, newN+1:end), C2, mb, nb);
-        X = [X1, X2];
+        C(:, 1:newN) = rtrsyst(A, B(1:newN, 1:newN), C(:, 1:newN), mb, nb);
+        C(:, newN+1:end) = gemm(C(:, 1:newN), B(1:newN, newN+1:end), C(:, newN+1:end));
+        C(:, newN+1:end) = rtrsyst(A, B(newN+1:end, newN+1:end), C(:, newN+1:end), mb, nb);
         
     else
         newM = newdims(A); newN = newdims(B);
         
-        X21 = rtrsyst(A(newM+1:end, newM+1:end), B(1:newN, 1:newN), C(newM+1:end, 1:newN), mb, nb);
-        C22 = gemm(X21, B(1:newN, newN+1:end), C(newM+1:end, newN+1:end));
-        C11 = gemm(-A(1:newM, newM+1:end), X21, C(1:newM, 1:newN));
-        X22 = rtrsyst(A(newM+1:end, newM+1:end), B(newN+1:end, newN+1:end), C22, mb, nb);
-        X11 = rtrsyst(A(1:newM, 1:newM), B(1:newN, 1:newN), C11, mb, nb);
-        C12 = gemm(-A(1:newM, newM+1:end), X22, C(1:newM, newN+1:end));
-        C12 = gemm(X11, B(1:newN, newN+1:end), C12);
-        X12 = rtrsyst(A(1:newM, 1:newM), B(newN+1:end, newN+1:end), C12, mb, nb);
-        X = [X11, X12; X21, X22];     
+        C(newM+1:end, 1:newN) = rtrsyst(A(newM+1:end, newM+1:end), B(1:newN, 1:newN), C(newM+1:end, 1:newN), mb, nb);
+        C(newM+1:end, newN+1:end) = gemm(C(newM+1:end, 1:newN), B(1:newN, newN+1:end), C(newM+1:end, newN+1:end)); % the 2 gemms are parallelizable
+        C(1:newM, 1:newN) = gemm(-A(1:newM, newM+1:end), C(newM+1:end, 1:newN), C(1:newM, 1:newN));
+        C(newM+1:end, newN+1:end) = rtrsyst(A(newM+1:end, newM+1:end), B(newN+1:end, newN+1:end), C(newM+1:end, newN+1:end), mb, nb);
+        C(1:newM, 1:newN) = rtrsyst(A(1:newM, 1:newM), B(1:newN, 1:newN), C(1:newM, 1:newN), mb, nb);
+        C(1:newM, newN+1:end) = gemm(-A(1:newM, newM+1:end), C(newM+1:end, newN+1:end), C(1:newM, newN+1:end));
+        C(1:newM, newN+1:end) = gemm(C(1:newM, 1:newN), B(1:newN, newN+1:end), C(1:newM, newN+1:end));
+        C(1:newM, newN+1:end) = rtrsyst(A(1:newM, 1:newM), B(newN+1:end, newN+1:end), C(1:newM, newN+1:end), mb, nb);
     end
     
     
